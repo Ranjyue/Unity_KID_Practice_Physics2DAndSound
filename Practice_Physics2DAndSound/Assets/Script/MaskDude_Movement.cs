8f9a50a9eef6210f2c4b9c2f3369d133aae693af
Anastasia Ranjyue <ranjyue@gmail.com>
2021-07-16 21:17:00 +0800

    赫綵遊戲開發班 - 作業文件_Fork 分支功能
    -為物件添加物理元件完成與網頁完成版相同效果
    -添加背景音樂與按鈕點擊音效
    -上傳至 Git Hub
    -發布網頁版並取得 Git Hub Pages
    
    \ 製作並且套用 animator 的 scale如何同步
    \ 不小心拉到鏡頭導致場景物看不到，怎麼辦?
    \設定程式卻沒有反應，為甚麼?

---
+++ Practice_Physics2DAndSound/Assets/Script/MaskDude_Movement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskDude_Movement : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private bool Grounded;
+
+    private int Health = 5;
+
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+        }
+
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up*JumpForce);
+    }
+
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+    }
+
+    public void Hit()
+    {
+        Health = Health - 1;
+        if (Health == 0) Destroy(gameObject);
+    }
+
+}
